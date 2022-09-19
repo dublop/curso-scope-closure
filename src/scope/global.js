097f174388edd3911a627e1461940b4fea06e61e
dublop <dnlopezavila@gmail.com>
2022-09-19 07:50:29 -0400

    First commit

---
+++ src/scope/global.js
@@ -0,0 +1,16 @@
+//Global scope
+var fruit = 'Apple';
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries() {
+    country = 'Colombia'; //global
+    console.log(country);
+}
+
+countries();
+console.log(country);
