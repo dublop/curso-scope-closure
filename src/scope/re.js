1bc822ed2154d3de2b171914bef4f439850d8952
dublop <dnlopezavila@gmail.com>
2022-09-19 09:01:40 -0400

    Add reassignment and redeclaration class

---
+++ src/scope/re.js
@@ -0,0 +1,36 @@
+var firstName; // undefined
+firstName = 'Dubraska';
+console.log(firstName);
+
+var lastName = 'Jorge'; // declarar - asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'David';  // declarar - asignar
+var secondName = 'Ana'; // reasignar
+console.log(secondName);
+
+// Let
+let fruit = 'Apple'; // declarar - asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// Const
+const animal = 'Dog'; // declarar - asignar
+animal = 'Cat';
+//const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('Toyota');
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
+
+const array = ["manzana", "pera"]
+array.push('Mandarina')
+console.log(array)
+// const array = [1,2,3] 
