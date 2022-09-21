2225f6e04fd1bf5f1ed61aab8bede7ed5c20cfee
dublop <dnlopezavila@gmail.com>
2022-09-21 08:47:53 -0400

    Add hoisting class

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+/*var nameOfDog;
+console.log(nameOfDog);
+var nameOfDog = 'Momo';
+console.log(nameOfDog);*/
+
+var momo = 'Momito';
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${momo}`);
+} 
+
+debugger;
